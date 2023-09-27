2aaf42c83d909e6e67457bb33a15b5621d71e77d
Ben Purinton <ben.purinton@gmail.com>
2023-09-27 20:14:40 +0000

    update render-start to ruby app.rb

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -5,10 +5,10 @@ set -o errexit
 # Uncomment the line depending on the framework you are deploying
 
 # Static HTML
-bundle exec rackup
+# bundle exec rackup
 
 # Sinatra
-# bundle exec ruby app.rb
+bundle exec ruby app.rb
 
 # Ruby on Rails
 # bundle exec puma -C config/puma.rb
